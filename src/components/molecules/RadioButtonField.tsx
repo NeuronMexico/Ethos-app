5cdce9d4aee27ab2fcbdb85703331856f8ea1f8d
Emmanuel <emmanuelsalazarid@gmail.com>
2023-09-13 11:46:27 -0600

    fix alignments

--- src/components/molecules/RadioButtonField.tsx
+++ src/components/molecules/RadioButtonField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RadioButton, Text } from 'components/atoms';
 import { FontWeightTypes, TypographyTypes } from '../atoms/CustomText';
 import { Container } from '../atoms/Container';
@@ -26,48 +26,52 @@ const RadioButtonField: React.FC<Props> = ({
   marginVertical = 8,
   value,
   caption,
-}: Props) => (
-  <Container style={{ marginVertical }}>
-    <Touchable onPress={() => onChange(!selected)} opacityEffect>
-      <Container row center={!caption}>
-        <RadioButton
-          selected={selected}
-        />
-        <Container flex>
-          <Container row center style={{ flexGrow: 1 }}>
-            <Container flex>
+}: Props) => {
+  const [centerText, setCenterText] = useState<boolean>(true);
+
+  return (
+    <Container style={{ marginVertical }}>
+      <Touchable onPress={() => onChange(!selected)} opacityEffect>
+        <Container row center={!caption && centerText}>
+          <RadioButton
+            selected={selected}
+          />
+          <Container flex>
+            <Container row center={centerText} style={{ flexGrow: 1 }}>
+              <Container flex>
+                <Text
+                  text={label}
+                  typography={typography}
+                  marginLeft={8}
+                  fontSize={fontSize}
+                  fontWeight={fontWeight}
+                  onTextLayout={({ nativeEvent: { lines } }) => setCenterText(lines.length <= 1)}
+                />
+              </Container>
+              {!!value && (
               <Text
-                text={label}
+                text={value}
                 typography={typography}
                 marginLeft={8}
                 fontSize={fontSize}
                 fontWeight={fontWeight}
-                numberOfLines={1}
               />
+              )}
             </Container>
-            {!!value && (
+            {!!caption && (
             <Text
-              text={value}
-              typography={typography}
+              text={caption}
+              typography="subtitle"
+              fontWeight="Regular"
               marginLeft={8}
-              fontSize={fontSize}
-              fontWeight={fontWeight}
+              marginTop={12}
             />
             )}
           </Container>
-          {!!caption && (
-          <Text
-            text={caption}
-            typography="subtitle"
-            fontWeight="Regular"
-            marginLeft={8}
-            marginTop={12}
-          />
-          )}
         </Container>
-      </Container>
-    </Touchable>
-  </Container>
-);
+      </Touchable>
+    </Container>
+  );
+};
 
 export { RadioButtonField };
