dba1fd6a8878e5faac72e1b4ab8f3e1f5446672c
Emmanuel <emmanuelsalazarid@gmail.com>
2023-10-03 17:24:05 -0600

    update DotIndicator component

--- src/components/molecules/DotIndicator.tsx
+++ src/components/molecules/DotIndicator.tsx
@@ -1,18 +1,25 @@
 import { Container } from 'components/atoms';
 import React from 'react';
-import { Animated, StyleSheet } from 'react-native';
+import { Animated, ColorValue, StyleSheet } from 'react-native';
 import Theme from 'theme';
 
 interface Props {
   totalDots: number;
   currentIndex: number;
+  inactiveColor?: ColorValue;
+  activeColor?: ColorValue;
 }
 
 const DOT_SIZE = 8;
 const ACTIVE_DOT_SIZE = 4 * DOT_SIZE;
 const MARGIN_HORIZONTAL = 4;
 
-const DotIndicator = ({ totalDots, currentIndex }: Props) => {
+const DotIndicator = ({
+  totalDots,
+  currentIndex,
+  inactiveColor = Theme.Colors.PlaceboBlue,
+  activeColor = Theme.Colors.SpringBouquet,
+}: Props) => {
   const dotContainerWidth = totalDots * (DOT_SIZE + 2 * MARGIN_HORIZONTAL);
 
   const renderDots = () => Array.from({ length: totalDots }).map((_, i) => {
@@ -23,7 +30,7 @@ const DotIndicator = ({ totalDots, currentIndex }: Props) => {
       height: DOT_SIZE,
       marginHorizontal: MARGIN_HORIZONTAL,
       borderRadius: isActive ? ACTIVE_DOT_SIZE / 2 : DOT_SIZE / 2,
-      backgroundColor: isActive ? Theme.Colors.SpringBouquet : Theme.Colors.PlaceboBlue,
+      backgroundColor: isActive ? activeColor : inactiveColor,
     };
 
     return <Animated.View key={i} style={[styles.dot, dotStyle]} />;
