b96cbf6436fa4f5f5c5f9e902a15a5e831002b84
Lessly <leslyzuniga.zc@gmail.com>
2023-10-03 23:58:14 -0600

    profileStack is added

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -55,3 +55,23 @@ export type PreviewStackParams = {
   Slider: undefined;
   SwipeableSwitch: undefined;
 };
+
+export type ConfirmationCodeScreenParams = {
+  type: string;
+  value: string;
+};
+
+export type ProfileStackParams = {
+  Profile: undefined;
+  ProfileEdit: undefined;
+  ConfirmationCode: {
+    type: string;
+    value: string
+  };
+};
+
+export type FileType = {
+  file: string;
+  fileName: string;
+  fileType: string;
+};
