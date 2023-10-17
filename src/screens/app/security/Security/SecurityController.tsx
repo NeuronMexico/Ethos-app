c4ad939621ac71123eefeb90b398fa52853fa753
Lessly <leslyzuniga.zc@gmail.com>
2023-10-17 12:36:54 -0600

    biometrics are added to change password

--- src/screens/app/security/Security/SecurityController.tsx
+++ src/screens/app/security/Security/SecurityController.tsx
@@ -1,14 +1,25 @@
 import { SafeArea } from 'components';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import ReactNativeBiometrics from 'react-native-biometrics';
 import { ProfileStackParams } from 'utils';
+import { useTranslation } from 'react-i18next';
 import SecurityScreen from './SecurityScreen';
 
+const rnBiometrics = new ReactNativeBiometrics({ allowDeviceCredentials: true });
+
 const SecurityController: React.FC = () => {
+  const { t } = useTranslation();
   const { navigate } = useNavigation<NativeStackNavigationProp<ProfileStackParams>>();
 
-  const onChangePassword = () => { navigate('ChangePassword'); };
+  const onChangePassword = useCallback(async () => {
+    const result = await rnBiometrics.simplePrompt({ promptMessage: t('global:confirmYourIdentity') });
+    if (result.success) {
+      navigate('ChangePassword');
+    }
+  }, [navigate, t]);
+
   return (
     <SafeArea>
       <SecurityScreen onChangePassword={onChangePassword} />
