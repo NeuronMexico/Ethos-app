4487829cdc529aa5d224a827f86b6c3652977dfd
Emmanuel <emmanuelsalazarid@gmail.com>
2023-10-23 14:47:31 -0600

    ScheduledPayments screen added

--- src/screens/app/transactions/ScheduledPayments/ScheduledPaymentsController.tsx
+++ src/screens/app/transactions/ScheduledPayments/ScheduledPaymentsController.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch } from 'reactRedux';
 import { SafeArea } from 'components';
+import { useBottomSheet } from 'context';
 import ScheduledPaymentsScreen from './ScheduledPaymentsScreen';
+import { PaymentBottomSheetContent } from './components';
 
 const ScheduledPaymentsController: React.FC = () => {
   const dispatch = useDispatch();
 
+  const bottomSheet = useBottomSheet();
+
+  const onPressPayment = useCallback(() => {
+    bottomSheet.show(<PaymentBottomSheetContent onPressDelete={bottomSheet.hide} onPressEdit={bottomSheet.hide} />);
+  }, [bottomSheet]);
+
   return (
     <SafeArea>
-      <ScheduledPaymentsScreen />
+      <ScheduledPaymentsScreen onPressPayment={onPressPayment} />
     </SafeArea>
   );
 };
--- src/screens/app/transactions/ScheduledPayments/ScheduledPaymentsScreen.tsx
+++ src/screens/app/transactions/ScheduledPayments/ScheduledPaymentsScreen.tsx
@@ -1,25 +1,86 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Text } from 'components';
+import { ScrollView } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import {
+  Container, Header, MultipleTextButton, Text,
+} from 'components';
+import { MoreIcon } from 'assets/svg';
+import Theme from 'theme';
+import { formatDate, formatQuantity } from 'utils';
 
 interface Props {
-  prop?: string
+  onPressPayment: () => void;
 }
 
-const ScheduledPaymentsScreen: React.FC<Props> = (props: Props) => {
-  const { prop } = props;
+const ScheduledPaymentsScreen: React.FC<Props> = ({ onPressPayment }) => {
+  const { t } = useTranslation();
 
   return (
-    <Container flex middle>
-      <Text text="ScheduledPaymentsScreen" />
+    <Container flex>
+      <Header title={t('transactions:scheduledPayments')} rightIcon={<MoreIcon width={22} height={22} />} />
+      <ScrollView
+        style={{ flex: 1, marginTop: 4 }}
+        contentContainerStyle={{ paddingHorizontal: Theme.Sizes.Padding, paddingTop: 28 }}
+      >
+        <Text
+          text={formatDate(new Date(), 'MMMM dd, yyyy')}
+          typography="subtitle"
+          fontWeight="Semibold"
+          textColor={Theme.Colors.GreatFalls}
+          transform="capitalize"
+        />
+        <MultipleTextButton
+          title="Pago a Mario Telles"
+          label={t('transactions:singlePayment')}
+          rightText={formatQuantity(2000)}
+          borderColor={Theme.Colors.PlaceboBlue}
+          borderRadius={24}
+          marginTop={16}
+          alignContent="space-between"
+          onPress={onPressPayment}
+        />
+        <MultipleTextButton
+          onPress={onPressPayment}
+          title="Pago de TDC ** *334"
+          label={t('transactions:directDebit')}
+          rightText={formatQuantity(3957)}
+          borderColor={Theme.Colors.PlaceboBlue}
+          borderRadius={24}
+          marginTop={16}
+          alignContent="space-between"
+        />
+        <MultipleTextButton
+          onPress={onPressPayment}
+          title="Netflix"
+          label={t('transactions:recurringPayment')}
+          rightText={formatQuantity(299)}
+          borderColor={Theme.Colors.PlaceboBlue}
+          borderRadius={24}
+          marginTop={16}
+          alignContent="space-between"
+        />
+
+        <Text
+          text={formatDate(new Date(2023, 5, 23), 'MMMM dd, yyyy')}
+          typography="subtitle"
+          fontWeight="Semibold"
+          textColor={Theme.Colors.GreatFalls}
+          transform="capitalize"
+          marginTop={16}
+        />
+        <MultipleTextButton
+          onPress={onPressPayment}
+          title="Pago Mariana"
+          label={t('transactions:singlePayment')}
+          rightText={formatQuantity(974)}
+          borderColor={Theme.Colors.PlaceboBlue}
+          borderRadius={24}
+          marginTop={16}
+          alignContent="space-between"
+        />
+      </ScrollView>
     </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 14
-  }
-});
 
 export default ScheduledPaymentsScreen;
