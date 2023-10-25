13c4564d903597f8e007873fd9a1ece18d46aab0
Lessly <leslyzuniga.zc@gmail.com>
2023-10-24 22:50:00 -0600

    add DownloadButtons component

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -3,3 +3,5 @@ export * from './Alert';
 export * from './ProfileEditForm';
 export * from './ButtonFieldEdit';
 export * from './EditProfileDataForm';
+export * from './TabExpensesSection';
+export * from './DownloadButtons';
