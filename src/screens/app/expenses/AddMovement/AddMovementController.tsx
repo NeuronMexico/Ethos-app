1228341274a517b779bc0440e777e89d7f6806f2
Lessly <leslyzuniga.zc@gmail.com>
2023-10-25 18:47:26 -0600

    add goback function in onSubmit button

--- src/screens/app/expenses/AddMovement/AddMovementController.tsx
+++ src/screens/app/expenses/AddMovement/AddMovementController.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { SafeArea } from 'components';
+import { useNavigation } from '@react-navigation/native';
 import AddMovementScreen from './AddMovementScreen';
 
-const AddMovementController: React.FC = () => (
-  <SafeArea>
-    <AddMovementScreen onSubmit={() => {}} />
-  </SafeArea>
-);
+const AddMovementController: React.FC = () => {
+  const { goBack } = useNavigation();
 
+  return (
+    <SafeArea>
+      <AddMovementScreen onSubmit={() => goBack()} />
+    </SafeArea>
+  );
+};
 export default AddMovementController;
