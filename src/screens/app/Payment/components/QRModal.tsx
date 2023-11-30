3fd8596d10f32ecdb9b3fd5450367ff6ed476fd7
Lessly <leslyzuniga.zc@gmail.com>
2023-11-30 12:03:23 +0100

    qr modal is updated

--- src/screens/app/Payment/components/QRModal.tsx
+++ src/screens/app/Payment/components/QRModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import {
   Button,
@@ -7,19 +7,17 @@ import {
   Header,
   Modal,
   QRCode,
-  SafeArea,
   Text,
 } from 'components';
 import Theme from 'theme';
-import { PaymentFlowType, formatDate, formatQuantity } from 'utils';
-import { ExportIcon } from 'assets/svg';
+import { PaymentFlowType, formatDate } from 'utils';
+import { ExportIcon, VisaIcon } from 'assets/svg';
 
 interface Props {
   visible: boolean;
   title: string;
   message: string;
   amount: string;
-  transactionCost: number;
   flow: PaymentFlowType;
   onPressCheckEstablishment: () => void;
   onPressBack: () => void;
@@ -30,7 +28,6 @@ const QRModal: React.FC<Props> = ({
   title,
   message,
   amount,
-  transactionCost,
   flow,
   onPressCheckEstablishment,
   onPressBack,
@@ -38,36 +35,23 @@ const QRModal: React.FC<Props> = ({
   const { t } = useTranslation();
 
   return (
-    <Modal visible={visible} animationType="slide">
-      <SafeArea>
-        <Header title={title} showBackButton={false} />
-
+    <Modal visible={visible} blur={false} animationType="slide" customBackgroundColor={Theme.Colors.PlaceboBlue}>
+      <Container flex middle style={{ paddingHorizontal: 16 }}>
         <ScrollView
-          contentContainerStyle={{
-            paddingHorizontal: Theme.Sizes.Padding,
-            paddingTop: Theme.Sizes.MarginTop,
-            alignItems: 'center',
-            flexGrow: 1,
-          }}
+          style={styles.scrollView}
+          contentContainerStyle={styles.contentContainer}
           showsVerticalScrollIndicator={false}
           bounces={false}
         >
+          <Header title={title} showBackButton={false} />
           {flow === 'code-payment' && (
-          <Text marginBottom={16}>
+          <Text>
             <Text text={t('alert:invoice')} typography="subtitle" fontWeight="Medium" />
             {' '}
             <Text text="43743" typography="subtitle" fontWeight="Bold" />
           </Text>
           )}
           <Text text={formatDate(new Date())} transform="capitalize" typography="subtitle" />
-          <Text marginVertical={16}>
-            <Text text={t('cards:codeValidFor')} typography="subtitle" fontWeight="Medium" />
-            {' '}
-            <Text text={t('cards:hours', { hours: 24 })} typography="subtitle" fontWeight="Bold" />
-          </Text>
-
-          <QRCode value="Ethos QR" />
-
           <Text
             text={message}
             typography="header"
@@ -75,19 +59,20 @@ const QRModal: React.FC<Props> = ({
             textAlign="center"
             fontWeight="Regular"
           />
-          <Text text="328572" fontSize={34} fontWeight="Bold" />
+          <Text text="328572" fontSize={34} fontWeight="Bold" marginBottom={16} />
+          <QRCode value="Ethos QR" />
           <Text
             text={amount}
             typography="header"
             fontWeight="Bold"
+            fontSize={34}
             marginVertical={16}
           />
           <Text>
-            <Text text={t('cards:transactionCost')} typography="caption" fontWeight="Medium" />
+            <Text text={t('cards:codeValidFor')} typography="subtitle" fontWeight="Medium" />
             {' '}
-            <Text text={formatQuantity(transactionCost)} typography="caption" fontWeight="Bold" />
+            <Text text={t('cards:hours', { hours: 24 })} typography="subtitle" fontWeight="Bold" />
           </Text>
-
           <Container flex alignment="end" center width="100%">
             {flow === 'personal-project-payment' && (
             <Button
@@ -107,36 +92,66 @@ const QRModal: React.FC<Props> = ({
             />
             )}
             {flow !== 'personal-project-payment' && (
-            <Button
-              label={t('global:goBack')}
-              onPress={onPressBack}
-              marginVertical={16}
-              backgroundColor={Theme.Colors.WhiteSmoke}
-              textColor={Theme.Colors.DarkSoul}
-            />
-            )}
-            {flow === 'code-payment' && (
-            <Button
-              label={t('global:share')}
-              onPress={() => {}}
-              width={40}
-              height={40}
-              borderRadius={14}
-              backgroundColor={Theme.Colors.PlaceboBlue}
-              icon={<ExportIcon />}
-              outsideLabel
-              fontSize={13}
-              fontWeight="Semibold"
-              outsideWidth={70}
-              colorless
-              marginBottom={16}
-            />
+            <>
+              <Button
+                label="**** **** **** 531"
+                onPress={() => {}}
+                backgroundColor={Theme.Colors.PlaceboBlue}
+                icon={<VisaIcon />}
+                colorless
+                paddingVertical={10}
+                marginTop={27}
+                marginBottom={16}
+                width="95%"
+              />
+              <Container
+                style={{
+                  width: '90%',
+                  height: 0,
+                  borderBottomWidth: 1,
+                  borderBottomColor: Theme.Colors.PlaceboBlue,
+                }}
+              />
+              <Button
+                label={t('form:goToTransactions')}
+                onPress={onPressBack}
+                marginVertical={16}
+                backgroundColor={Theme.Colors.WhiteSmoke}
+                textColor={Theme.Colors.DarkSoul}
+              />
+              <Button
+                label={t('global:share')}
+                onPress={onPressBack}
+                icon={<ExportIcon color={Theme.Colors.White} />}
+              />
+            </>
             )}
           </Container>
         </ScrollView>
-      </SafeArea>
+      </Container>
     </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Theme.Colors.White,
+    borderRadius: 16,
+    width: '100%',
+    maxHeight: '87%',
+    flexGrow: 0,
+    overflow: 'hidden',
+  },
+  contentContainer: {
+    // paddingTop: 32,
+    paddingBottom: 24,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+  },
+  ethosCreditLogo: {
+    marginTop: 16,
+    marginBottom: 16,
+  },
+});
 
 export { QRModal };
--- src/screens/app/transactions/CashPayment/CashPaymentController.tsx
+++ src/screens/app/transactions/CashPayment/CashPaymentController.tsx
@@ -1,19 +1,104 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { useDispatch } from 'reactRedux';
-import { SafeArea } from 'components';
+import {
+  Button, Container, SafeArea, Text,
+} from 'components';
+import { QRModal } from 'screens/app/Payment/components';
+import { useAlert } from 'context';
+import { useTranslation } from 'react-i18next';
+import { formatQuantity } from 'utils';
+import { VisaIcon } from 'assets/svg';
+import Theme from 'theme';
+import ReactNativeBiometrics from 'react-native-biometrics';
 import CashPaymentScreen from './CashPaymentScreen';
 
-const CashPaymentController: React.FC = () => {
-  const dispatch = useDispatch();
-  const { navigate } = useNavigation<NativeStackNavigationProp<any>>();
+const rnBiometrics = new ReactNativeBiometrics({ allowDeviceCredentials: true });
 
+const CashPaymentController: React.FC = () => {
+  const { navigate } = useNavigation<NativeStackNavigationProp<any>>();
+  const alert = useAlert();
+  const { t } = useTranslation();
+  const [showQRModal, setShowQRModal] = useState<boolean>(false);
+
+  const biometricManager = useCallback(async () => {
+    const result = await rnBiometrics.simplePrompt({ promptMessage: t('global:confirmYourIdentity') });
+    if (result.success) {
+      setShowQRModal(true);
+    }
+  }, [t]);
+
+  const generateCode = () => {
+    alert.show({
+      extraInfo: (
+        <>
+          <Text
+            text={formatQuantity(Number('00'))}
+            textAlign="center"
+            fontWeight="Bold"
+            typography="header"
+            fontSize={34}
+            marginBottom={16}
+          />
+          <Text text="Tarjeta de donde saldrá el dinero:" textAlign="center" />
+          <Button
+            label="**** **** **** 531"
+            onPress={() => {}}
+            backgroundColor={Theme.Colors.PlaceboBlue}
+            icon={<VisaIcon />}
+            colorless
+            paddingVertical={10}
+            paddingHorizontal={80}
+            marginTop={27}
+            marginBottom={16}
+          />
+          <Container
+            style={{
+              width: 'auto',
+              height: 0,
+              borderBottomWidth: 1,
+              borderBottomColor: Theme.Colors.PlaceboBlue,
+            }}
+          />
+        </>
+      ),
+      title: t('charges:confirmCharge'),
+      actions: [
+        {
+          label: t('global:confirm'),
+          type: 'secondary',
+          onPress: () => {
+            alert.hide();
+            biometricManager();
+          },
+        },
+        {
+          label: t('global:cancel'),
+          type: 'primary',
+          onPress: () => {
+            alert.hide();
+          },
+        },
+      ],
+    });
+  };
   return (
     <SafeArea>
       <CashPaymentScreen
-        onPressGenerateQR={() => navigate('PaymentFlow', { flow: 'code-payment' })}
+        onSubmit={generateCode}
         onPressEstablishments={() => navigate('Establishments')}
+      />
+      <QRModal
+        visible={showQRModal}
+        title={t('transactions:payToQR')}
+        message={t('transactions:goToAffiliatedStore')}
+        amount={`${formatQuantity(500)} MXN`}
+        flow="code-payment"
+        onPressCheckEstablishment={() => {
+          setShowQRModal(false);
+          navigate('Establishments');
+        }}
+        onPressBack={() => setShowQRModal(false)}
       />
     </SafeArea>
   );
