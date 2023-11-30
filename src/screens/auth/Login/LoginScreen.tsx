a7baeece358abfe9308da1acf5a883641026b636
Edgardo Victorino Marin <edgardo.victorino@gmail.com>
2023-11-30 13:50:16 -0600

    feat: adding login screen

--- src/screens/auth/Login/LoginScreen.tsx
+++ src/screens/auth/Login/LoginScreen.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Text } from 'components';
+import { Container } from 'components';
+import Theme from 'theme';
+import { EthosIcon } from 'assets/svg';
+import LoginForm from './components/LoginForm';
 
 interface Props {
   prop?: string
@@ -8,17 +11,28 @@ interface Props {
 
 const LoginScreen: React.FC<Props> = (props: Props) => {
   const { prop } = props;
+  const { container } = styles;
 
   return (
-    <Container flex middle>
-      <Text text="LoginScreen" />
+    <Container flex style={container}>
+      <Container style={{ marginTop: Theme.Sizes.MarginTop }}>
+        <EthosIcon />
+      </Container>
+      <LoginForm
+        onPressForgotPassword={() => {}}
+        onPressLogin={() => {}}
+        onPressBiometricLogin={() => {}}
+        onPressRegister={() => {}}
+        onPressTerms={() => {}}
+        onPressPrivacy={() => {}}
+      />
     </Container>
   );
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 14,
+  container: {
+    paddingHorizontal: Theme.Sizes.Padding,
   },
 });
 
