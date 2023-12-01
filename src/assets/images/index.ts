88a50dfb530754c9c663821ce283c86041c98bfa
Lessly <leslyzuniga.zc@gmail.com>
2023-12-01 11:36:51 +0100

    tutorial modal is added

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,9 +3,15 @@ import ETHOS_LOGO from './ethos-logo.png';
 import CARD_REVERSE from './card-reverse.png';
 import ETHOS_CREDIT_LOGO from './ethos-credit-logo.png';
 
+import TUTORIAL_SECOND from './tutorial-second-img.png';
+import TUTORIAL_THIRD from './tutorial-third-img.png';
+
 export {
   TELCEL_LOGO,
   ETHOS_LOGO,
   CARD_REVERSE,
   ETHOS_CREDIT_LOGO,
+
+  TUTORIAL_SECOND,
+  TUTORIAL_THIRD,
 };
