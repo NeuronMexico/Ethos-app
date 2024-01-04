28c90756a5c4c9495775a861694ae0c8442a06d9
Lessly <leslyzuniga.zc@gmail.com>
2024-01-04 11:37:56 -0500

    add packages imgs

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,6 +5,12 @@ import ETHOS_CREDIT_LOGO from './ethos-credit-logo.png';
 import ETHOS_CREDIT_CARDS from './ethos-credit-cards.png';
 import DUMMY_CARD from './dummy-card.png';
 import GIFT_IMG from './gift-img.png';
+import FAMILY_IMG from './family-img.png';
+import OFF_IMG from './25-off-img.png';
+import BIKE_IMG from './bike-img.png';
+import OLDER_PEOPLE_IMG from './older-people-img.png';
+import PETS_IMG from './pets-img.png';
+import PREGNANT_IMG from './pregnant-img.png';
 
 import TUTORIAL_FIRST from './tutorial-first-img.png';
 import TUTORIAL_SECOND from './tutorial-second-img.png';
@@ -23,6 +29,12 @@ export {
   ETHOS_CREDIT_LOGO,
   ETHOS_CREDIT_CARDS,
   GIFT_IMG,
+  FAMILY_IMG,
+  OFF_IMG,
+  BIKE_IMG,
+  OLDER_PEOPLE_IMG,
+  PETS_IMG,
+  PREGNANT_IMG,
 
   TUTORIAL_FIRST,
   TUTORIAL_SECOND,
